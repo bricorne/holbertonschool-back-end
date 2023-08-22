2e4adac6ae14bd2b3a3e38e55280cfdc7068cb96
bricorne <bricepoitevineau@gmail.com>
2023-08-22 05:08:54 -0700

    pep8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,21 +4,23 @@
 import requests
 import sys
 
+
 def get_employee_todo_progress(emp_id):
-        base_url = (f'https://jsonplaceholder.typicode.com/users/{emp_id}')	 
-        emp_data = requests.get(base_url).json()
-        todos_url = (f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}')
+    base_url = (f'https://jsonplaceholder.typicode.com/users/{emp_id}')	 
+    emp_data = requests.get(base_url).json()
+    todos_url = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}')
 
-        total_tasks = requests.get(todos_url).json()
+    total_tasks = requests.get(todos_url).json()
 
-        done_tasks = [task for task in total_tasks if task.get("completed")]
+    done_tasks = [task for task in total_tasks if task.get("completed")]
 
-        print(
-            f"Employee {emp_data['name']} is done with "
-            f"tasks({len(done_tasks)}/{len(total_tasks)}):"
-        )
-        for task in done_tasks:
-            print("\t", task["title"])
+    print(
+        f"Employee {emp_data['name']} is done with "
+        f"tasks({len(done_tasks)}/{len(total_tasks)}):"
+    )
+    for task in done_tasks:
+        print("\t", task["title"])
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
