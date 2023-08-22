63bfd91d18bbbf21e199755bfa0fc14fd97ecc59
Brice Poitevineau <bricorne@MacBook-Air-de-Brice.local>
2023-08-22 15:04:42 +0200

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-'''
-Python script that returns information using REST API
-'''
+'''Python script that returns information using REST API'''
+
 import requests
 import sys
 
-if __name__ == "__main__":
-    get_emp_id = sys.argv[1]
-    user_url = (f'https://jsonplaceholder.typicode.com/users/{get_emp_id}')
-    get_emp_data = requests.get(user_url).json()
-    todos_url = (
-        f'https://jsonplaceholder.typicode.com/todos?userId={get_emp_id}')
-    get_emp_tasks = requests.get(todos_url).json()
 
-    done_tasks = [task for task in get_emp_tasks if task.get("completed")]
+def get_employee_todo_progress(emp_id):
+    base_url = (f'https://jsonplaceholder.typicode.com/users/{emp_id}')
+    emp_data = requests.get(base_url).json()
+    todos_url = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={emp_id}')
+
+    total_tasks = requests.get(todos_url).json()
+
+    done_tasks = [task for task in total_tasks if task.get("completed")]
 
     print(
-        f"Employee {get_emp_data['name']} is done with "
-        f"tasks({len(done_tasks)}/{len(get_emp_tasks)}):"
+        f"Employee {emp_data['name']} is done with "
+        f"tasks({len(done_tasks)}/{len(total_tasks)}):"
     )
     for task in done_tasks:
         print("\t", task["title"])
+
+
+if __name__ == "__main__":
+    emp_id = int(sys.argv[1])
+    get_employee_todo_progress(emp_id)
